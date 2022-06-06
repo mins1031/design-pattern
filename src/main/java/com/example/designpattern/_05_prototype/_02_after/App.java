c74057b30b2b7c71a6d8662d76c800a436bf9071
mins1031 <urisegea@naver.com>
2022-06-06 23:41:25 +0900

    프로토 타입 패턴 학습

---
+++ src/main/java/com/example/designpattern/_05_prototype/_02_after/App.java
@@ -0,0 +1,24 @@
+package com.example.designpattern._05_prototype._02_after;
+
+public class App {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        GithubRepository repository = new GithubRepository();
+        repository.setUser("whiteship");
+        repository.setName("live-study");
+
+        GithubIssue githubIssue = new GithubIssue(repository);
+        githubIssue.setId(1);
+        githubIssue.setTitle("1주차 내용 정리 : jvm");
+
+        GithubIssue clone = (GithubIssue) githubIssue.clone();
+        System.out.println(clone);
+
+        //TODO clone 을 사용할때 이득을 보는 경우는 모제품을 만들떄의 리소스 소모가 큰경우(ex. 타 서버와 통신을 해서 가져온 데이터로 만들어야 하거나, 디비에서 가져온 데이터로 뭔가 만들어야 할 경우 )
+        System.out.println(clone != githubIssue);
+        System.out.println(clone.equals(githubIssue));
+        System.out.println(clone.getClass() == githubIssue.getClass());
+
+        //TODO clone이 기본적으로 제공하는 복사는 얕은 복사임. 그래서 clone 인스턴스는 repository의 변경에 영향을 받게 된다.
+        System.out.println(clone.getRepository() == githubIssue.getRepository());
+    }
+}
---
+++ src/main/java/com/example/designpattern/_05_prototype/_02_after/GithubIssue.java
@@ -0,0 +1,58 @@
+package com.example.designpattern._05_prototype._02_after;
+
+import java.util.Objects;
+
+public class GithubIssue implements Cloneable{
+
+    private int id;
+
+    private String title;
+
+    private GithubRepository repository;
+
+    public GithubIssue(GithubRepository repository) {
+        this.repository = repository;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public GithubRepository getRepository() {
+        return repository;
+    }
+
+    public void setRepository(GithubRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GithubIssue that = (GithubIssue) o;
+        return id == that.id && Objects.equals(title, that.title) && Objects.equals(repository, that.repository);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, repository);
+    }
+}
