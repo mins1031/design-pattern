b0f084ca46d3b7d39ebdd29159a805075ee942dd
mins1031 <urisegea@naver.com>
2022-10-06 00:38:12 +0900

    feat : headfirst 옵저버 패턴 개선 전

---
+++ src/main/java/com/example/designpattern/book_headfirst/_02_observer/before/WeatherData.java
@@ -0,0 +1,38 @@
+package com.example.designpattern.book_headfirst._02_observer.before;
+
+public class WeatherData {
+
+    private float temperature;
+    private float humidity;
+    private float pressure;
+
+    public WeatherData(float temperature, float humidity, float pressure) {
+        this.temperature = temperature;
+        this.humidity = humidity;
+        this.pressure = pressure;
+    }
+
+    //밑의 메서드는 특정 인터페이스를 구현하고는 있지만 구현에 의존하고 있기에 변경에 취약하다.
+    public void measurementsChanged() {
+        float temperature = getTemperature();
+        float humidity = getHumidity();
+        float pressure = getPressure();
+
+        //밑의 3줄을 캡슐화 해줘야한다. -> 옵저버 패턴 적용. 근데 전략패턴을 사용해도 되지 않나?
+        currentConditionsDisplay.update(temperature, humidity, pressure);
+        statisticsDisplay.update(temperature, humidity, pressure);
+        forecastDisplay.update(temperature, humidity, pressure);
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+}
