b61c7098fb9b3101e72d32346b31ea452239d973
mins1031 <urisegea@naver.com>
2022-11-07 22:03:27 +0900

    feat : state 패턴 1

---
+++ src/main/java/com/example/designpattern/book_headfirst/_10_state/GumballMachine.java
@@ -0,0 +1,81 @@
+package com.example.designpattern.book_headfirst._10_state;
+
+public class GumballMachine {
+    private static final int SOLD_OUT = 0;
+    private static final int NO_QUARTER = 1;
+    private static final int HAS_QUARTER = 2;
+    private static final int SOLD = 3;
+
+    private int state = SOLD_OUT;
+    private int count = 0;
+
+    public GumballMachine(int count) {
+        this.count = count;
+        if (count > 0) {
+            state = NO_QUARTER;
+        }
+    }
+
+    public void insertQuarter() {
+        if (state == HAS_QUARTER) {
+            System.out.println("동전은 한개만 넣어주세요.");
+        } else if (state == NO_QUARTER) {
+            state = HAS_QUARTER;
+            System.out.println("동전을 넣으셨습니다.");
+        } else if (state == SOLD_OUT) {
+            System.out.println("매진되었습니다.");
+        } else if (state == SOLD) {
+            System.out.println("알맹이를 내보내고 있습니다.");
+        }
+    }
+
+    public void ejectQuarter() {
+        if (state == HAS_QUARTER) {
+            System.out.println("동전이 반환됩니다.");
+        } else if (state == NO_QUARTER) {
+            state = HAS_QUARTER;
+            System.out.println("동전을 넣어 주세요.");
+        } else if (state == SOLD_OUT) {
+            System.out.println("이미 알맹이를 뽑으셨습니다.");
+        } else if (state == SOLD) {
+            System.out.println("동전을 넣지 않으셨습니다. 동전이 반환되지 않습니다.");
+        }
+    }
+
+    public void turnCrank() {
+        if (state == HAS_QUARTER) {
+            System.out.println("손잡이는 한번만 돌려주세요.");
+        } else if (state == NO_QUARTER) {
+            state = HAS_QUARTER;
+            System.out.println("동전을 넣어 주세요.");
+        } else if (state == SOLD_OUT) {
+            System.out.println("매진되었습니다.");
+        } else if (state == SOLD) {
+            System.out.println("손잡이를 돌리셨습니다");
+            state = SOLD;
+            dispense();
+        }
+    }
+
+    public void dispense() {
+        if (state == HAS_QUARTER) {
+            System.out.println("알맹이를 내보내고 있습니다.");
+            count = count - 1;
+            if (count == 0) {
+                System.out.println("더이상 알맹이가 없습니다.");
+                state = SOLD_OUT;
+            } else {
+                state = NO_QUARTER;
+            }
+        } else if (state == NO_QUARTER) {
+            state = HAS_QUARTER;
+            System.out.println("동전을 넣어주세요.");
+        } else if (state == SOLD_OUT) {
+            System.out.println("매진입니다.");
+        } else if (state == SOLD) {
+            System.out.println("알맹이를 내보낼수 없습니다.");
+        }
+    }
+
+
+}
